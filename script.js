3368561c8cdc73a8e135d09ad4b3f56c9c05529e
sabrinaacham <sabrinaacham@gmail.com>
2021-12-10 13:02:07 -0400

    html

---
+++ script.js
@@ -0,0 +1,6 @@
+//To practice DOM manipulation, we want to create a web page for seeding a crypto wallet.
+//Crypto wallets typically have a phrase of 12 words which allow the owners of the wallet private access to its content.
+// Create a web page with 12 HTML elements of the same class and use JavaScript to populate them.
+// In your JavaScript create an array of 24 words and then use a query selector to return all the 12 elements as a NodeList.
+//Finally, use a FOR loop to iterate through the NodeList and insert into the HTML a random word from the array of words
+//Bonus: For your random word selection, ensure that no word is repeated
